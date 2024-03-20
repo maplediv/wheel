ff817731a4ff7d178fa1c8d391f6afb03405ab18
Coder0379 <147442566+Coder0379@users.noreply.github.com>
2024-03-20 16:37:42 -0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   ],
   server: {
     port: 10000, // Specify the port number
+    host: '0.0.0.0'
   },
   build: {
     outDir: 'dist', // Specify the output directory
