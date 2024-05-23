f7ddaff0831f4b2b0551d37088e4f68771bf3b37
Coder0379 <147442566+Coder0379@users.noreply.github.com>
2024-05-23 15:06:41 -0700

    Message describing your changes

--- src/server.cjs
+++ src/server.cjs
@@ -36,7 +36,7 @@ app.post('/register', async (req, res, next) => {
   }
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
