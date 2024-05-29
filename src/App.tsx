f8c7ddfbd41969bf8de60be2951ed2fbc33c50eb
Coder0379 <147442566+Coder0379@users.noreply.github.com>
2024-05-29 12:37:59 -0700

    Message describing your changes

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,10 @@ const HomePage = () => {
           <div className="col-md-6 order-md-1">
             <div className="text-container">
               <div className="text-left">
-                <h2>The Artist needs <br />tools to create</h2>
+                <h2>The artist needs tools to create</h2>
                 <p className="responsive-text">
-                  This is some paragraph text. This is some paragraph text. <br />This is some paragraph text. This is some paragraph text. <br />This is some paragraph text. This is some paragraph text. <br />This is some paragraph text. This is some paragraph text.<br /> This is some paragraph text. This is some paragraph text. <br />This is some paragraph text. This is some paragraph text.
+                Visual artists can elevate their creative process by using our app's sophisticated color image recognition feature. <br />
+                By uploading images, artists can seamlessly identify and analyze the color palette within their artwork, enabling them to experiment with new hues and shades. <br />
                 </p>
                 <button className="home-button btn btn-primary" onClick={() => { window.location.href = '/paint'; }}>Try Now</button>
               </div>
@@ -44,9 +45,11 @@ const HomePage = () => {
           <div className="col-md-6 order-md-4">
             <div className="text-container">
               <div className="text-left">
-                <h2>This is some H2 <br />text</h2>
+                <h2> <br />Infinite imagination</h2>
                 <p className="responsive-text">
-                  This is some paragraph text. This is some paragraph text. <br />This is some paragraph text. This is some paragraph text. <br />This is some paragraph text. This is some paragraph text. <br />This is some paragraph text. This is some paragraph text.<br /> This is some paragraph text. This is some paragraph text. <br />This is some paragraph text. This is some paragraph text.
+                This tool not only assists in color matching but also offers insights into the dominant colors and their proportions, helping artists maintain harmony and balance in their compositions.  <br />
+                Whether refining a piece or seeking inspiration, our app empowers artists to harness the power of color with precision and ease. <br />
+                
                 </p>
                 <button className="home-button btn btn-primary" onClick={() => { window.location.href = '/paint'; }}>Learn More</button>
               </div>
@@ -89,15 +92,17 @@ const PaintPage = () => {
             <div className="color-canvas">
               <h1 className="account-heading">Color Canvas</h1>
               <p className="color-text">
-                Click the "Choose File" button and upload any file to return the dominant colors in the image. <br />
-                Click the "Analyze Image" button to initialize the image analysis processing.<br />
-                Color Canvas supports a variety of image file types, including but not limited to:
+                <ul>
+               <li> Click the "Choose File" button and upload any file to return the dominant colors in the image.</li> <br />
+               <li>Click the "Analyze Image" button to initialize the image analysis processing.</li><br />
+               <li>Color Canvas supports a variety of image file types, including but not limited to:
                 JPEG,
                 PNG,
                 GIF (including animated GIFs),
                 BMP,
                 TIFF,
-                WEBP.
+                WEBP.</li>
+                </ul>
               </p>
               {/* Display VisionAPIComponent only on the paint page */}
               <VisionAPIComponent onColorResponse={handleColorResponse} />
