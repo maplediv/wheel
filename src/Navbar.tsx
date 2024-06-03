d7c03b01a5e5f2eac1b2b39f92421d7ea796b79f
Coder0379 <147442566+Coder0379@users.noreply.github.com>
2024-06-03 14:31:12 -0700

    Message describing your changes

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,55 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { handleLoginIconClick } from './globalEvents'; // Import the event handler function
 
 const Navbar: React.FC = () => {
+  const [userFirstName, setUserFirstName] = useState<string | null>(null); // State variable to store user's first name
+
   return (
-   
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">
-        <div className="logo-container">
-          <img src="/src/images/art.svg" alt="Logo" width="100" height="100" />
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <div className="logo-container">
+            <img src="/src/images/art.svg" alt="Logo" width="100" height="100" />
+          </div>
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/paint">
+                Paint
+              </a>
+            </li>
+            <li className="nav-item">
+              {userFirstName ? (
+                <span className="nav-link">Welcome, {userFirstName}</span>
+              ) : (
+                <a className="nav-link" href="#" id="loginIcon" onClick={handleLoginIconClick}>
+                  <img src="/src/images/login.svg" alt="Login" width="24" height="24" />
+                </a>
+              )}
+            </li>
+          </ul>
         </div>
-      </a>
-     
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="/">
-          
-          
-          
-              Home
-            </a>
-          </li>
-          <li className="nav-item ">
-            <a className="nav-link" href="/paint">
-              Paint
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#" id="loginIcon" onClick={handleLoginIconClick}>
-              <img src="/src/images/login.svg" alt="Login" width="24" height="24" />
-            </a>
-          </li>
-        </ul>
-
-        
       </div>
-    </div>
-  </nav>
+    </nav>
   );
 };
 
