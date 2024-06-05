aa95751043b7c0dd97b19dff1e4c056bc7a040d4
Coder0379 <147442566+Coder0379@users.noreply.github.com>
2024-06-05 16:46:08 -0700

    Message describing your changes

--- src/Login.tsx
+++ src/Login.tsx
@@ -5,45 +5,11 @@ interface LoginPageProps {
 }
 
 const LoginPage: React.FC<LoginPageProps> = ({ handleSuccessfulLogin }) => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [showLoginForm, setShowLoginForm] = useState(true); // Default to login form initially
+  const [showLoginForm, setShowLoginForm] = useState(true);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [showErrorMessage, setShowErrorMessage] = useState('');
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    try {
-      const response = await fetch('https://wheelback.onrender.com/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ firstName, lastName, email, password }),
-      });
-
-      if (response.ok) {
-        console.log('User registered successfully');
-        setShowSuccessMessage(true);
-        setShowErrorMessage('');
-        setFirstName('');
-        setLastName('');
-        setEmail('');
-        setPassword('');
-      } else {
-        const data = await response.json();
-        console.log('Failed to register user:', data.message);
-        setShowErrorMessage(data.message);
-        setShowSuccessMessage(false);
-      }
-    } catch (error) {
-      console.log('Error registering user:', error);
-      setShowErrorMessage('Error registering user');
-      setShowSuccessMessage(false);
-    }
-  };
 
   const handleLoginFormSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -91,30 +57,6 @@ const LoginPage: React.FC<LoginPageProps> = ({ handleSuccessfulLogin }) => {
           <h2>{showLoginForm ? 'Login' : 'Create Account'}</h2>
           <div className="form-container">
             <form onSubmit={showLoginForm ? handleLoginFormSubmit : handleSubmit}>
-              {!showLoginForm && (
-                <>
-                  <div className="mb-3">
-                    <input
-                      type="text"
-                      value={firstName}
-                      onChange={(e) => setFirstName(e.target.value)}
-                      className="form-control"
-                      placeholder="First Name"
-                      required
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <input
-                      type="text"
-                      value={lastName}
-                      onChange={(e) => setLastName(e.target.value)}
-                      className="form-control"
-                      placeholder="Last Name"
-                      required
-                    />
-                  </div>
-                </>
-              )}
               <div className="mb-3">
                 <input
                   type="email"
